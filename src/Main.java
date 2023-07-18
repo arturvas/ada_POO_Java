b3cbe24b8cec01e5c0c23f52b1f8617314d751fe
arturvas <artur.vasconcelos8@gmail.com>
2023-07-17 21:21:26 -0400

    test static var implement

--- src/Main.java
+++ src/Main.java
@@ -5,10 +5,13 @@ public class Main {
     public static void main(String[] args) {
 
         Dog dog1 = new Dog("Tufão", "Marrom com bege", 15, 27, 10, "carinho");
+        System.out.println(dog1.getNumberOfDogs()); // testing static variable
+
         Dog dog2 = new Dog("Bela", "Caramelo", 18, 35, 11, "nada");
+        System.out.println(dog2.getNumberOfDogs()); // testing static variable
+        System.out.println(dog1.getNumberOfDogs()); // testing static variable
 
         System.out.println(dog1.getName() + " pegou uma " + dog1.take());
-
         System.out.println(dog2.getName() + " está " + dog2.interact("grito"));
         System.out.println(dog2.getName() + " está " + dog2.interact("nada"));
         System.out.println(dog1.getName() + " está " + dog1.interact("carinho"));
