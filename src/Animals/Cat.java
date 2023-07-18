1f2653e58a6d2da5d3f58e7c21263479c2b8ca35
arturvas <artur.vasconcelos8@gmail.com>
2023-07-18 09:42:25 -0400

    created `Cat` class

--- src/Animals/Cat.java
+++ src/Animals/Cat.java
@@ -1,2 +1,39 @@
-package Animals;public class Cat {
+package Animals;
+
+public class Cat {
+
+//    attributes
+    static int numberOfCats;
+    private String name;
+    private String color;
+    private int height;
+    private double weight;
+    private String mood;
+
+//    constructors
+    public Cat(String name, String color, int height, double weight, String mood) {
+        this.name = name;
+        this.color = color;
+        this.height = height;
+        this.weight = weight;
+        this.mood = mood;
+
+        numberOfCats ++;
+    }
+
+//    methods
+    void eat(){}
+
+    void sleep(){}
+
+    void sound(){
+        System.out.println("Miau Miau");
+    }
+
+    @Override
+    public String toString() {
+        return "Cat{" +
+                "name='" + name + '\'' +
+                '}';
+    }
 }
