3da08772c74312deac180cce6ffa875397aae532
arturvas <artur.vasconcelos8@gmail.com>
2023-07-18 09:41:58 -0400

    created `Bird` class

---
+++ src/Animals/Bird.java
@@ -0,0 +1,33 @@
+package Animals;
+
+public class Bird {
+
+//    attributes
+    static int numberOfBirds;
+    private String name;
+    private String color;
+    private int height;
+    private double weight;
+    private String mood;
+
+//    constructors
+    public Bird(String name, String color, int height, double weight, String mood) {
+        this.name = name;
+        this.color = color;
+        this.height = height;
+        this.weight = weight;
+        this.mood = mood;
+
+        numberOfBirds ++;
+    }
+
+//    methods
+    void eat(){}
+
+    void sleep(){}
+
+    void sound(){
+        System.out.println("Piu Piu");
+    }
+
+}
